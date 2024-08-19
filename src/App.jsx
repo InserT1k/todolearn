9b3bf29ea8962453033dd2aaa73160ce80307e64
InserT1k <64252942+InserT1k@users.noreply.github.com>
2024-08-19 11:40:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import EmojiVote from './EmojiVote';
 import Results from './Results';
 import './App.css';
 
-function App() {
-  const [votes, setVotes] = useState([]);
-  const [showResults, setShowResults] = useState(false);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: [],
+      showResults: false,
+    };
+  }
 
-  const handleVotes = (newVotes) => {
-    setVotes(newVotes);
+  handleVotes = (newVotes) => {
+    this.setState({ votes: newVotes });
   };
 
-  const clearVotes = () => {
+  setShowResults = (show) => {
+    this.setState({ showResults: show });
+  };
+
+  clearVotes = () => {
     localStorage.removeItem('emojiVotes');
-    setShowResults(false);
+    this.setState({ showResults: false });
     window.location.reload();
   };
 
-  return (
-    <div className="app">
-      <h1>Голосування за найкращий смайлик</h1>
-      <EmojiVote onVote={handleVotes} />
-      <button className="show-results-button" onClick={() => setShowResults(true)}>
-        Show Results
-      </button>
-      {showResults && <Results votes={votes} />}
-      <button className="clear-results-button" onClick={clearVotes}>
-        Clear Results
-      </button>
-    </div>
-  );
+  render() {
+    const { votes, showResults } = this.state;
+
+    return (
+      <div className="app">
+        <h1>Голосування за найкращий смайлик</h1>
+        <EmojiVote onVote={this.handleVotes} />
+        <button className="show-results-button" onClick={() => this.setShowResults(true)}>
+          Show Results
+        </button>
+        {showResults && <Results votes={votes} />}
+        <button className="clear-results-button" onClick={this.clearVotes}>
+          Clear Results
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
