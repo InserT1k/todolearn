31cf7f6175d6343cce93cfb2496c379dcb78daaa
InserT1k <64252942+InserT1k@users.noreply.github.com>
2024-08-19 11:40:50 +0300

    Update Results.jsx

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Results = ({ votes }) => {
-  const winner = votes.reduce((prev, current) =>
-    prev.votes > current.votes ? prev : current
-  );
+class Results extends Component {
+  render() {
+    const { votes } = this.props;
+    const winner = votes.reduce((prev, current) =>
+      prev.votes > current.votes ? prev : current
+    );
 
-  return (
-    <div>
-      <h3>Результати голосування:</h3>
-      <p>Переможець: {winner.emoji}</p>
-      <p>Кількість голосів: {winner.votes}</p>
-    </div>
-  );
-};
+    return (
+      <div>
+        <h3>Результати голосування:</h3>
+        <p>Переможець: {winner.emoji}</p>
+        <p>Кількість голосів: {winner.votes}</p>
+      </div>
+    );
+  }
+}
 
 export default Results;
